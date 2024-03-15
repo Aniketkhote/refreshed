0467d77e8679e04ba6f786b87695286679a4e2f0
Aniket Khote <aniketkhote99@gmail.com>
2024-03-15 12:56:17 +0530

    Refactor example code

--- example/lib/pages/home/domain/adapters/repository_adapter.dart
+++
@@ -1,6 +0,0 @@
-import '../entity/cases_model.dart';
-
-// ignore: one_member_abstracts
-abstract class IHomeRepository {
-  Future<CasesModel> getCases();
-}
--- example/lib/shared/logger/logger_utils.dart
+++
@@ -1,7 +0,0 @@
-mixin Logger {
-  // Sample of abstract logging function
-  static void write(String text, {bool isError = false}) {
-    // ignore: avoid_print
-    Future.microtask(() => print('** $text. isError: [$isError]'));
-  }
-}
